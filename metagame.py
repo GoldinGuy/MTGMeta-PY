08922952db743cbdd1467181cb9222675a8b59a9
Seth <sethgoldin27@gmail.com>
2020-09-22 01:12:31 -0400

    simplify code

--- metagame.py
+++ metagame.py
@@ -6,13 +6,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+# GLOBAL FUNCS
 def card_names(_deck):
     return [card[1] for card in _deck]
 
 
-def most_common_cards(deck, k):
-    deck.sort(key=lambda deck: deck[0], reverse=True)
-    return [card[1] for card in deck[:k]]
+def most_common_cards(_deck, k):
+    _deck.sort(key=lambda _deck: _deck[0], reverse=True)
+    return [card[1] for card in _deck[:k]]
 
 
 def decks_by_label(a_label):
@@ -38,15 +39,16 @@ def distance(x, y):
 # CUSTOMIZABLE GLOBAL VARS
 NUM_CLUSTERS = 20
 NUM_TOP_VERS = 15
-# FORMATS = ['pauper', 'legacy', 'modern', 'standard']
-FORMATS = ['standard']
-
+FORMATS = ['modern', 'legacy', 'pauper']
 IGNORE = ['Island', 'Forest', 'Mountain', 'Swamp', 'Plains']
 
+
 for FORMAT in FORMATS:
-    # DEFINE OUTPUT JSON
     decks = []
+    all_cards = []
+    cards_w_ig = []
     unique_cards = []
+    # DEFINE OUTPUT JSON
     format_json = {
         'archetypes': [],
         'format_top_cards': [],
@@ -59,56 +61,44 @@ for FORMAT in FORMATS:
         decks_json = json.load(f)
 
     # CONVERT JSON FILE TO LIST W/ PROPER FORMATTING
-    json_index = 0
     for deck in decks_json:
-        json_index += 1
-        try:
-            # if deck['main'][0]['name'] is not None:
-            cards_in_deck = []
-            for card in deck['main']:
-                card_name = str(card.get('name')).replace("'", "\\'")
-                quantity = int(card['quantity'])
-                cards_in_deck.append([quantity, card_name])
-                if any(card_name in d for d in unique_cards):
-                    unique_cards[next((i for i,d in enumerate(unique_cards) if card_name in d), None)][card_name] += quantity
-                elif card_name not in IGNORE:
-                    unique_cards.append({card_name: quantity})
-            decks.append(cards_in_deck)
-        except Exception as e:
-            decks_json.pop(json_index)
+        cards_in_deck = []
+        for card in deck['main']:
+            card_name = str(card.get('name')).replace("'", "\\'")
+            quantity = int(card['quantity'])
+            cards_in_deck.append([quantity, card_name])
+            # FILL CARDS ARRAYS
+            all_cards.append(card_name)
+            if card_name not in IGNORE:
+                cards_w_ig.append(card_name)
+            if any(card_name in d for d in unique_cards):
+                unique_cards[next((i for i, d in enumerate(unique_cards) if card_name in d), None)][
+                    card_name] += quantity
+            elif card_name not in IGNORE:
+                unique_cards.append({card_name: quantity})
+        decks.append(cards_in_deck)
+    format_json['total_cards_parsed'] = len(all_cards)
+    all_cards = list(set(all_cards))
+    format_json['unique_cards_parsed'] = len(all_cards)
 
 
     def deck_to_vector(input_deck):
-        v = [0] * len(all_card_names)
-        for x, name in enumerate(all_card_names):
+        v = [0] * len(all_cards)
+        for x, name in enumerate(all_cards):
             for number, card_name in input_deck:
                 if card_name == name:
                     v[x] += number
         return v
 
-    # DETERMINE TOTAL PARSE DATA
-    all_card_names = []
-    for deck_card_names in [card_names(deck) for deck in decks]:
-        all_card_names += deck_card_names
-    format_json['total_cards_parsed'] = len(all_card_names)
-    all_card_names_no_basics_len = len([i for i in all_card_names if i not in IGNORE])
-    all_card_names = set(all_card_names)
-    format_json['unique_cards_parsed'] = len(all_card_names)
-    all_card_names = list(all_card_names)
-    all_card_names_no_basics = [i for i in all_card_names if i not in IGNORE]
 
     # K-MEANS CLUSTERING
     deck_vectors = [deck_to_vector(deck) for deck in decks]
-
     km = dask_ml.cluster.KMeans(n_clusters=NUM_CLUSTERS, oversampling_factor=5)
     km.fit(deck_vectors)
-
     labels = list(km.labels_.compute())
     decks_labels = list(zip(decks, labels))
-
     card_counts = [(card_label, len(decks_by_label(card_label))) for card_label in range(NUM_CLUSTERS)]
     total_instances = sum([count for _, count in card_counts])
-
     # FOR EACH ARCHETPYE IN FORMAT
     for CLUSTER_ID in range(NUM_CLUSTERS):
         # DETERMINE MOST COMMON CARDS IN CLUSTER
@@ -118,8 +108,7 @@ for FORMAT in FORMATS:
         card_set = set(card_set)
         # DETERMINE BEST-FIT NAME/DECK FOR CLUSTER
         cluster_name = 'Unknown'
-        best_fit_deck = []
-        best_fit_deck_sb = []
+        best_fit_deck = {'deck': [], 'sb': []}
         max_similar_cards = 0
         for deck in decks_json:
             num_similar_cards = 0
@@ -129,31 +118,28 @@ for FORMAT in FORMATS:
             if num_similar_cards > max_similar_cards:
                 max_similar_cards = num_similar_cards
                 cluster_name = str(deck['name'])
-                best_fit_deck = deck['main']
-                best_fit_deck_sb = deck['sb']
+                best_fit_deck['deck'] = deck['main']
+                best_fit_deck['sb'] = deck['sb']
         # PRINT CLUSTER
         print("Cluster #" + str(CLUSTER_ID) + " (" + cluster_name + ") :")
         print(card_set)
         print("\n")
 
         instances = (CLUSTER_ID, len(decks_by_label(CLUSTER_ID)))[1]
-
         deck_archetype = {
             'archetype_name': cluster_name,
             'top_cards': list(card_set),
             'metagame_percentage': "{:.2%}".format(instances / total_instances),
-            'best_fit_deck': {'main': best_fit_deck, 'sb': best_fit_deck_sb}
+            'best_fit_deck': best_fit_deck
         }
-
         format_json['archetypes'].append(deck_archetype)
 
     unique_cards = sorted(unique_cards, key=lambda s: s[next(iter(s))], reverse=True)
 
-
     def closest_cards(a_card, b):
         this_card = apparition_ratio(a_card)[0]
         distances = []
-        for name in all_card_names_no_basics:
+        for name in cards_w_ig:
             dist = distance(apparition_ratio(name)[0], this_card)
             distances.append((name, dist))
         distances.sort(key=lambda x: x[1])
@@ -163,7 +149,7 @@ for FORMAT in FORMATS:
 
     def versatile_cards(k):
         variances = []
-        for name in all_card_names_no_basics:
+        for name in cards_w_ig:
             versatility = sum([1 if x > 0 else 0 for x in apparition_ratio(name)[0]])
             variances.append((name, versatility))
         variances.sort(key=lambda x: x[1], reverse=True)
@@ -180,6 +166,7 @@ for FORMAT in FORMATS:
             i += 1
         return common_decks
 
+
     # DETERMINE TOP CARDS IN FORMAT
     for card_dict in unique_cards[:NUM_TOP_VERS]:
         card_name = list(card_dict.keys())[0]
@@ -189,7 +176,7 @@ for FORMAT in FORMATS:
             'common_archetypes': common_decks(card_name),
             'cards_found_with': closest_cards(card_name, 8),
             'total_instances': quantity,
-            'percentage_of_total_cards': "{:.2%}".format(quantity / all_card_names_no_basics_len),
+            'percentage_of_total_cards': "{:.2%}".format(quantity / len(cards_w_ig)),
         }
 
         format_json['format_top_cards'].append(top_card)
@@ -226,19 +213,19 @@ for FORMAT in FORMATS:
 # print("\nApparition ratio for " + cards_to_analyze[2] + "\n" + str(apparition_ratio(cards_to_analyze[2])))
 # print("\nApparition ratio for " + cards_to_analyze[3] + "\n" + str(apparition_ratio(cards_to_analyze[3])))
 
-    # graph data
-    # plt.rc('font', size=14)
-    # label_counts = [(label, len(decks_by_label(label))) for label in range(NUM_CLUSTERS)]
-    # counts = [count for _, count in label_counts]
-    # points = {
-    #     'cluster': [label for label, _ in label_counts],
-    #     'count': [count for _, count in label_counts],
-    # }
-    # sns.barplot(x="cluster", y="count", data=points, order=points['cluster']).set_title("# Decks by Cluster in " + FORMAT)
-    # plt.gca().spines['top'].set_visible(False)
-    # plt.gca().spines['right'].set_visible(False)
-    # plt.savefig('graphs/' + FORMAT + ' # Decks by Cluster')
-    # show()
+# graph data
+# plt.rc('font', size=14)
+# label_counts = [(label, len(decks_by_label(label))) for label in range(NUM_CLUSTERS)]
+# counts = [count for _, count in label_counts]
+# points = {
+#     'cluster': [label for label, _ in label_counts],
+#     'count': [count for _, count in label_counts],
+# }
+# sns.barplot(x="cluster", y="count", data=points, order=points['cluster']).set_title("# Decks by Cluster in " + FORMAT)
+# plt.gca().spines['top'].set_visible(False)
+# plt.gca().spines['right'].set_visible(False)
+# plt.savefig('graphs/' + FORMAT + ' # Decks by Cluster')
+# show()
 
 
 # cards to analyze % by cluster (pie chart)
